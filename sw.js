7567e1b7261cd31ad6a518a9668ce3b187552938
Антон Трошин <i@atroshin.ru>
2026-08-05 18:55:14 +0300

    fix: cache auto renewal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'otaman-v1';
+const CACHE = 'otaman-v2';
 const URLS = [
   'index.html',
   'style.css',
@@ -20,16 +20,27 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }))
-  );
+  const isNavigate = e.request.mode === 'navigate' || e.request.url.endsWith('sw.js');
+  if (isNavigate) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+    );
+  }
 });
